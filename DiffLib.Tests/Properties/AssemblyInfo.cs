45e7cac89767e63eaf501956d95c86dc02c31360
Lasse V. Karlsen <lasse@vkarlsen.no>
2010-11-28 17:53:21 -0100

    Initial commit, empty projects.

---
+++ DiffLib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DiffLib.Tests")]
+[assembly: AssemblyDescription("Unit-tests for DiffLib")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiffLib")]
+[assembly: AssemblyCopyright("Copyright (C) Lasse V. Karlsen 2010, All rights reversed")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1dc19fcf-efd0-4fed-b77e-13fb7d6cf0ae")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
---
+++ DiffLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DiffLib")]
+[assembly: AssemblyDescription("A Diff implementation in C# for .NET 4.0.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiffLib")]
+[assembly: AssemblyCopyright("Copyright (C) Lasse V. Karlsen 2010, All rights reversed")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("dedbf82f-9c9b-49c4-bc62-ad9164e3c6ec")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
