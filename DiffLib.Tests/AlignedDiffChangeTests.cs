31c339d4c414970464751b0244213ebee2f74872
Lasse V. Karlsen <lasse@vkarlsen.no>
2010-11-28 23:18:30 -0100

    Added some more unit-tests for AlignedDiffChange and DiffSection

---
+++ DiffLib.Tests/AlignedDiffChangeTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace DiffLib.Tests
+{
+    [TestFixture]
+    public class AlignedDiffChangeTests
+    {
+        [TestCase(ChangeType.Added, "", "")]
+        [TestCase(ChangeType.Deleted, "", "")]
+        [TestCase(ChangeType.Changed, "", "")]
+        [TestCase(ChangeType.Same, "", "")]
+        [TestCase(ChangeType.Added, "xx", "")]
+        [TestCase(ChangeType.Added, "yy", "")]
+        [TestCase(ChangeType.Added, "", "xx")]
+        [TestCase(ChangeType.Added, "", "yy")]
+        public void Constructor_InitializesPropertiesCorrectly(ChangeType type, string element1, string element2)
+        {
+            var adc = new AlignedDiffChange<string>(type, element1, element2);
+            Assert.That(adc.Type, Is.EqualTo(type));
+            Assert.That(adc.Element1, Is.EqualTo(element1));
+            Assert.That(adc.Element2, Is.EqualTo(element2));
+        }
+    }
+}
---
+++ DiffLib.Tests/DiffSectionTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DiffLib.Tests
+{
+    [TestFixture]
+    public class DiffSectionTests
+    {
+        [TestCase(true, 1, 1)]
+        [TestCase(false, 1, 1)]
+        [TestCase(true, 3, 3)]
+        [TestCase(false, 5, 7)]
+        public void Constructor_InitializesPropertiesCorrectly(bool equal, int length1, int length2)
+        {
+            var ds = new DiffSection(equal, length1, length2);
+
+            Assert.That(ds.Equal, Is.EqualTo(equal));
+            Assert.That(ds.Length1, Is.EqualTo(length1));
+            Assert.That(ds.Length2, Is.EqualTo(length2));
+        }
+
+        [TestCase(true, 1, 1)]
+        [TestCase(false, 1, 1)]
+        [TestCase(true, 3, 3)]
+        [TestCase(false, 5, 7)]
+        public void TwoInstances_WithSameProperties_CompareEqual(bool equal, int length1, int length2)
+        {
+            var ds1 = new DiffSection(equal, length1, length2);
+            var ds2 = new DiffSection(equal, length1, length2);
+
+            Assert.That(ds1.Equals(ds2), Is.True);
+        }
+
+        [TestCase(true, 1, 1)]
+        [TestCase(false, 1, 1)]
+        [TestCase(true, 3, 3)]
+        [TestCase(false, 5, 7)]
+        public void OneInstances_ComparesEqualToItself(bool equal, int length1, int length2)
+        {
+            var ds = new DiffSection(equal, length1, length2);
+
+            Assert.That(ds.Equals(ds), Is.True);
+        }
+
+        [TestCase(true, 1, 1)]
+        [TestCase(false, 1, 1)]
+        [TestCase(true, 3, 3)]
+        [TestCase(false, 5, 7)]
+        public void OneInstances_ComparesUnequalToNull(bool equal, int length1, int length2)
+        {
+            var ds = new DiffSection(equal, length1, length2);
+
+            Assert.That(ds.Equals(null), Is.False);
+        }
+
+        [TestCase(true, 1, 1)]
+        [TestCase(false, 1, 1)]
+        [TestCase(true, 3, 3)]
+        [TestCase(false, 5, 7)]
+        public void OneInstances_ComparesEqualToItselfThroughObjectEquals(bool equal, int length1, int length2)
+        {
+            var ds = new DiffSection(equal, length1, length2);
+
+            Assert.That(ds.Equals((object) ds), Is.True);
+        }
+
+        [TestCase(true, 1, 1)]
+        [TestCase(false, 1, 1)]
+        [TestCase(true, 3, 3)]
+        [TestCase(false, 5, 7)]
+        public void TwoInstances_WithSameProperties_CompareEqualThroughObjectEquals(bool equal, int length1, int length2)
+        {
+            var ds1 = new DiffSection(equal, length1, length2);
+            object ds2 = new DiffSection(equal, length1, length2);
+
+            Assert.That(ds1.Equals(ds2), Is.True);
+        }
+
+        [TestCase(true, 1, 1)]
+        [TestCase(false, 1, 1)]
+        [TestCase(true, 3, 3)]
+        [TestCase(false, 5, 7)]
+        public void TwoInstances_WithSameProperties_ProduceSameHashCodes(bool equal, int length1, int length2)
+        {
+            var ds1 = new DiffSection(equal, length1, length2);
+            var ds2 = new DiffSection(equal, length1, length2);
+
+            Assert.That(ds1.GetHashCode(), Is.EqualTo(ds2.GetHashCode()));
+        }
+
+        [Test]
+        public void Constructor_DifferentLengthsWhenEqual_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new DiffSection(true, 1, 2));
+        }
+
+        [Test]
+        public void Constructor_NegativeLength1_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new DiffSection(false, -1, 1));
+        }
+
+        [Test]
+        public void Constructor_NegativeLength2_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new DiffSection(false, 1, -1));
+        }
+    }
+}
