22bb1811ca9d2c3aaba416075fd43b215b47e440
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2015-07-23 14:01:58 +0200

    Fixed performance regression with new implementation.

---
+++ DiffLib/HashcodeOccurance.cs
@@ -0,0 +1,22 @@
+﻿namespace DiffLib
+{
+    internal class HashcodeOccurance
+    {
+        public HashcodeOccurance(int position, HashcodeOccurance next)
+        {
+            Position = position;
+            Next = next;
+        }
+
+        public int Position
+        {
+            get;
+        }
+
+        public HashcodeOccurance Next
+        {
+            get;
+            set;
+        }
+    }
+}
