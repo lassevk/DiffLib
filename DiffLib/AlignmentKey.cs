8d064aa5efb03d28fe0da147163d0a0158262c2d
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2015-07-23 11:48:22 +0200

    Added unit-tests for AlignmentKey. Also changed AlignmentKey to expose positions as auto-properties.

--- DiffLib/AlignmentKey.cs
+++ DiffLib/AlignmentKey.cs
@@ -4,19 +4,26 @@ namespace DiffLib
 {
     internal struct AlignmentKey : IEquatable<AlignmentKey>
     {
-        private readonly int _Position1;
-        private readonly int _Position2;
-
         public AlignmentKey(int position1, int position2)
         {
-            _Position1 = position1;
-            _Position2 = position2;
+            Position1 = position1;
+            Position2 = position2;
         }
 
-        public bool Equals(AlignmentKey other) => _Position1 == other._Position1 && _Position2 == other._Position2;
+        public int Position1
+        {
+            get;
+        }
+
+        public int Position2
+        {
+            get;
+        }
+
+        public bool Equals(AlignmentKey other) => Position1 == other.Position1 && Position2 == other.Position2;
 
         public override bool Equals(object obj) => !ReferenceEquals(null, obj) && (obj is AlignmentKey && Equals((AlignmentKey)obj));
 
-        public override int GetHashCode() => unchecked((_Position1 * 397) ^ _Position2);
+        public override int GetHashCode() => unchecked((Position1 * 397) ^ Position2);
     }
 }
