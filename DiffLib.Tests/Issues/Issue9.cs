4b69b6975e7bb0ee9a39c6c37397c87be24f1955
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2016-04-21 10:21:09 +0200

    This fixes issue #9.

---
+++ DiffLib.Tests/Issues/Issue9.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+// ReSharper disable ReturnValueOfPureMethodIsNotUsed
+
+namespace DiffLib.Tests.Issues
+{
+    [TestFixture]
+    public class Issue9
+    {
+        [Test]
+        public void Repro()
+        {
+            var s1 = "904".ToCharArray();
+            var s2 = "448".ToCharArray();
+
+            Assert.DoesNotThrow(() => Diff.CalculateSections(s1, s2).ToList());
+        }
+    }
+}
