4125487185ee57eeda7ac1f90fd30e8b503d705f
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2017-04-09 20:26:28 +0200

    Replaced FinalBuilder script with batch file

---
+++ release.bat
@@ -0,0 +1,31 @@
+@echo off
+
+setlocal
+
+if "%SIGNINGKEYS%" == "" goto setup
+
+set year=%date:~6,4%
+set month=%date:~3,2%
+set day=%date:~0,2%
+set tm=%time:~0,2%%time:~3,2%
+
+copy "%SIGNINGKEYS%\Lasse V. Karlsen Private.snk" "Lasse V. Karlsen.snk"
+if errorlevel 1 goto error
+
+if exist DiffLib\bin rd /s /q DiffLib\bin
+if errorlevel 1 goto error
+
+msbuild DiffLib.sln /target:Clean,Rebuild /p:Configuration=Release /p:Version=%year%.%month%.%day%.%tm%
+if errorlevel 1 goto error
+
+copy DiffLib\bin\Release\DiffLib*.nupkg .\
+if errorlevel 1 goto error
+
+exit /B 0
+
+:error
+exit /B 1
+
+:setup
+echo Requires SIGNINGKEYS environment variable to be set
+exit /B 1
