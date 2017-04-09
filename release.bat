0d5186f7a4df8eac4fc4ec28f581da3dc2aa0605
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2017-04-09 21:38:12 +0200

    Release batch file now also pushes to nuget and tags the commit in the git repository

--- release.bat
+++ release.bat
@@ -2,12 +2,15 @@
 
 setlocal
 
+del *.nupkg
+if errorlevel 1 goto error
+
 if "%SIGNINGKEYS%" == "" goto setup
 
-set year=%date:~6,4%
-set month=%date:~3,2%
-set day=%date:~0,2%
-set tm=%time:~0,2%%time:~3,2%
+set /A year=%date:~6,4%
+set /A month=%date:~3,2%
+set /A day=%date:~0,2%
+set /A tm=%time:~0,2%%time:~3,2%
 
 copy "%SIGNINGKEYS%\Lasse V. Karlsen Private.snk" "DiffLib\Lasse V. Karlsen.snk"
 if errorlevel 1 goto error
@@ -18,13 +21,26 @@ if errorlevel 1 goto error
 nuget restore
 if errorlevel 1 goto error
 
-msbuild DiffLib\DiffLib.csproj /target:Clean,Rebuild /p:Configuration=Release /p:Version=%year%.%month%.%day%.%tm%
+set VERSION=%year%.%month%.%day%.%tm%
+msbuild DiffLib\DiffLib.csproj /target:Clean,Rebuild /p:Configuration=Release /p:Version=%VERSION%
 if errorlevel 1 goto error
 
 copy DiffLib\bin\Release\DiffLib*.nupkg .\
 if errorlevel 1 goto error
 
 git checkout "DiffLib\Lasse V. Karlsen.snk"
+
+echo=
+echo================================================
+set /P PUSHYESNO=Push package to nuget? [y/N]
+if "%PUSHYESNO%" == "Y" GOTO PUSH
+exit /B 0
+
+:PUSH
+nuget push DiffLib.%VERSION%.nupkg -Source https://www.nuget.org/api/v2/package
+if errorlevel 1 goto error
+git tag version/%VERSION%
+if errorlevel 1 goto error
 exit /B 0
 
 :error
@@ -36,3 +52,4 @@ goto exitwitherror
 
 :exitwitherror
 git checkout "DiffLib\Lasse V. Karlsen.snk"
+exit /B 1
