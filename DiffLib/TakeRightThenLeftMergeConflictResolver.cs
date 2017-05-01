bcb8c811d035635773513f6f8900e2796e4ecc78
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2017-05-01 14:15:09 +0200

    Fixes issue #13; Added right-centric versions of TakeLeftThenRight*MergeConflictResolver types

---
+++ DiffLib/TakeRightThenLeftMergeConflictResolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+namespace DiffLib
+{
+    /// <summary>
+    /// This implementation of <see cref="IMergeConflictResolver{T}"/> takes the left side then takes the right side.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of elements in the collections being merged.
+    /// </typeparam>
+    public class TakeRightThenLeftMergeConflictResolver<T> : IMergeConflictResolver<T>
+    {
+        /// <inheritdoc />
+        [NotNull, ItemCanBeNull]
+        public IEnumerable<T> Resolve([NotNull] IList<T> commonBase, [NotNull] IList<T> left, [NotNull] IList<T> right)
+        {
+            foreach (var item in right)
+                yield return item;
+
+            foreach (var item in left)
+                yield return item;
+        }
+    }
+}
